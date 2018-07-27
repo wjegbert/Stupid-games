b2c5be4618088d49766d3da1cfd088b14505765c
William <wegbert@crimson.ua.edu>
2018-07-27 17:41:52 -0500

    Add files via upload

--- CapsuleHead/Camera.gd
+++ CapsuleHead/Camera.gd
@@ -3,10 +3,12 @@ extends Camera
 # class member variables go here, for example:
 export var distance = 10.0
 export var height = 4.0
+var prev = Transform()
 
 func _ready():
 	set_physics_process(true)
 	set_as_toplevel(true)
+	prev = get_parent().get_global_transform().origin
 
 func _physics_process(delta):
 	var target = get_parent().get_global_transform().origin
@@ -18,3 +20,4 @@ func _physics_process(delta):
 	
 	pos = target + offset
 	look_at_from_position(pos, target, up)
+	prev = target
--- CapsuleHead/CapsuleHead.gd
+++ CapsuleHead/CapsuleHead.gd
@@ -4,7 +4,7 @@ extends KinematicBody
 var gravity = -9.8
 var velocity = Vector3()
 var camera
-
+var character 
 
 const SPEED = 6
 const ACCELL = 3
@@ -14,37 +14,39 @@ const DECELL = 5
 onready var anim = get_node("AnimationPlayer")
 
 func _ready():
-#	camera = get_node("Target/Camera").get_global_transform()
+	character = get_node(".")
 	pass
 	
 func _physics_process(delta):
 	var direction = Vector3()
 	camera = get_node("Body/Camera").get_global_transform()
-	
+	var is_moving = false
 	if Input.is_action_pressed("ui_up"):
 		direction -= camera.basis[2]
+		is_moving = true
 		if (anim.is_playing()!=true):
 			anim.play("Walking")
 	if Input.is_action_pressed("ui_down"):
 		direction += camera.basis[2]
+		is_moving = true
 		if (anim.is_playing()!=true):
 			anim.play("Walking")
 	if Input.is_action_pressed("ui_left"):
-		if Input.is_action_pressed("ui_page_down"):
-			rotate_y(deg2rad(60*delta))
-		else:
-			direction -= camera.basis[0]/2
-			rotate_y(deg2rad(35*delta))
-			if (anim.is_playing()!=true):
-				anim.play("Walking")
+		
+		direction -= camera.basis[0]
+#			rotate_y(deg2rad(35*delta))
+		is_moving = true
+		if (anim.is_playing()!=true):
+			anim.play("Walking")
 	if Input.is_action_pressed("ui_right"):
-		if Input.is_action_pressed("ui_page_down"):
-			rotate_y(deg2rad(-60*delta))
-		else:
-			direction += camera.basis[0]/2
-			rotate_y(deg2rad(-35*delta))
-			if (anim.is_playing()!=true):
-				anim.play("Walking")
+#		if Input.is_action_pressed("ui_page_down"):
+#			rotate_y(deg2rad(-60*delta))
+#		else:
+		direction += camera.basis[0]
+		is_moving = true
+#			rotate_y(deg2rad(-35*delta))
+		if (anim.is_playing()!=true):
+			anim.play("Walking")
 	
 	direction.y = 0
 	direction = direction.normalized()
@@ -67,6 +69,12 @@ func _physics_process(delta):
 	
 	velocity = move_and_slide(velocity, Vector3(0,1,0))
 	
+	if is_moving:
+		var angle = atan2(hv.x, hv.z)
+		var char_rot = character.get_rotation()
+		char_rot.y = angle
+		character.set_rotation(char_rot)
+		
 	if is_on_floor() and Input.is_key_pressed(KEY_SPACE):
 		velocity.y =10
 		
